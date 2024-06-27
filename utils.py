bc81569bd6227bf1dfc568856c22661843ec6cc9
suhaib <saa83@windowslive.com>
2024-06-27 17:54:16 +0300

    add write json operation function

--- utils.py
+++ utils.py
@@ -6,3 +6,6 @@ def load_json(filepath):
             return json.load(file)
     except FileNotFoundError:
         return {}
+def write_json(filepath, data):
+    with open(filepath, 'w') as file:
+        json.dump(data, file, indent=4)
